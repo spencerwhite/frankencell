a17584aa0fddd68b0728f9d4ec872c91ddc429ac
Spencer White <email@spencerwhite.xyz>
2023-02-06 09:20:36 -0500

    Add non-pub PhantomData to TokenBuilder and TokenWith
    prevents willy-nilly construction. Also remove getters and setters from
    `TokenWith` since they are no longer necessary

--- src/builder.rs
+++ src/builder.rs
@@ -1,6 +1,10 @@
+use std::marker::PhantomData;
+
 use crate::tokens::*;
 
-pub struct TokenBuilder<const ID: usize>;
+pub struct TokenBuilder<const ID: usize>(
+    PhantomData<()>, //prevents users outside the API from creating an instance of TokenBuilder
+);
 
 impl<const ID: usize> TokenBuilder<ID> {
     /// Creates a new `TokenBuilder` with this ID. 
@@ -10,7 +14,7 @@ impl<const ID: usize> TokenBuilder<ID> {
     /// represent mutable or immutable access to data, creating multiple `TokenBuilder`s with the
     /// same ID could mutably alias data.
     pub const unsafe fn new() -> Self {
-        Self
+        Self(PhantomData)
     }
 
     /// Convert this TokenBuilder into a [Token](crate::tokens::Token) as well as the next
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,8 +1,13 @@
+use std::marker::PhantomData;
+
 use crate::cells::Cell;
 
 /// A generic token that can store any data. See examples/arena.rs for an example of how this could
 /// be used.
-pub struct TokenWith<T, const ID: usize>(T);
+pub struct TokenWith<T, const ID: usize>(
+    T,
+    PhantomData<()>,
+);
 
 impl<T, const ID: usize> TokenWith<T, ID> {
     /// Creates a new token with this ID.
@@ -11,23 +16,11 @@ impl<T, const ID: usize> TokenWith<T, ID> {
     /// Because tokens represent access (mutable or immutable) to a memory location, creating >1
     /// tokens is equivalent to creating >1 mutable references to data.
     pub const unsafe fn new(t: T) -> Self {
-        Self(t)
+        Self(t, PhantomData)
     }
 
     pub const fn cell(&self, t: T) -> Cell<T, ID> {
         Cell::new(t)
-    }
-
-    /// Getters and setters are necessary so an API user can't manually create a Token and must
-    /// instead use [Self::new()]
-    pub fn get(&self) -> &T {
-        &self.0
-    }
-
-    /// Getters and setters are necessary so an API user can't manually create a Token and must
-    /// instead use [Self::new()]
-    pub fn get_mut(&mut self) -> &mut T {
-        &mut self.0
     }
 }
 
